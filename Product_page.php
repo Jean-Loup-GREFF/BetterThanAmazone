66065cbcd12770267bbd7f30400e0e0f32a479b4
Grand Corbeau <qboens@hotmail.fr>
2019-10-04 15:13:22 +0200

    arrivee_product_page

---
+++ Product_page.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Product Page</title>
+</head>
+<body>
+		<section>
+			<article>
+				<img src="image_essai.jpg">
+				
+			</article>
+			<aside>
+				<p id="Nom_prod">Produit 1</p>
+			</aside>
+			<aside>
+				<p id="Prix_prod">8000000€</p>
+			</aside>
+			<aside>
+				<p id="Desc_prod">
+					Le produit 1 est fabriqué par M. X
+					<br/>
+					Ses qualitées sont : 		<br/>-blabla 1
+												<br/>-blabla 2
+
+				</p>
+			</aside>
+			<aside>
+				<form method="post">
+					<input type="submit" value="Ajouter au panier">
+				</form>
+			</aside>
+		</section>
+</body>
+</html>
