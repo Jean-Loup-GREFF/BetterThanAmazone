e9a554b4f7e033f03f305432e9942f9f635aed1f
jean-loup <jeanloup.greff@gmail.com>
2019-10-04 16:33:45 +0200

    header/footer
    miss css

---
+++ footer.php
@@ -0,0 +1,4 @@
+<footer>
+        <div><a href="home.php">home</a></div>
+        <div><a href="www.isen.fr">sponso ISEN</a></div>
+</footer>
---
+++ header.php
@@ -0,0 +1,17 @@
+<header><a id="headertitle" href="home.php">BAmazone.com</a>
+<nav>
+<div>
+<a href="search.php">Search</a>
+</div>
+<div class="header">
+<a href="contact.php">Contact us</a>
+<p>
+      id/mail
+      <input type="text" name="id" value="mail addresse"/><br />
+      password
+      <input type="text" name="mdp" value="password"/><br />
+      <br />
+  </p>
+</div>
+</nav>
+</header>
