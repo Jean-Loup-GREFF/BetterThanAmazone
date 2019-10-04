dea532394620c693144ffb9e791480e16541d30c
Gabriel <takedadesmullier@gmail.com>
2019-10-04 17:12:42 +0200

    SearchProduct
    page d'affichage de tout les produits avec options de recherche. CSS à finir.

---
+++ SearchProductList.php
@@ -0,0 +1,82 @@
+<html> 
+<head>
+    <link rel="stylesheet" href=SearchProductList.css />
+</head>
+<body>
+    
+    <p><img src='imageacheter.png' /><br /></p>
+    <div id='searchbox'>
+        <h1>Recherche</h1>
+    <form><p>
+    Rechercher un produit dans le Store: 
+    <input type="text" name="search" class=string placeholder="Licorne volante..."/><br />
+</p></form>
+    <form><p>
+    
+    <label for="budget">Votre budget:</label>
+       <select name="budget" id="budget">
+           <option value="peuimporte">C'est peu important</option>
+           <option value="moins5">moins de 5 euros</option>
+           <option value="entre5et10">5-10 euros</option>
+           <option value="entre10et20">10-20 euros</option>
+           <option value="entre20et30">20-30 euros</option>
+           <option value="entre30et40">30-40 euros</option>
+           <option value="entre40et50">40-50 euros</option>
+           <option value="plusde50">plus de 50 euros</option>
+       </select>  
+</p>
+</form>
+<form><p>
+    
+    <label for="categorie">Catégorie:</label>
+       <select name="categorie" id="categorie">
+           <option value="peuimportecategorie">Tout les produits</option>
+           <option value="armes">Armes</option>
+           <option value="armures">Armures</option>
+           <option value="potions">Potions</option>
+           <option value="baguettesmagiques">Baguettes de sorcier</option>
+           <option value="livredesorts">Livre de Sortilèges</option>
+           <option value="creatures">Créatures</option>
+       </select>  
+</p>
+</form>
+</div>
+
+<TABLE>
+<CAPTION> Résultats </CAPTION>
+
+<TR>
+    <TD><p><a href='produit.'>Licorne volante</a><br /><img id='imageminiature' src='licorne.png' /><br /><img id='etoiles' src='5etoiles.png' /><br/> 15 euros<br/> livraison: 0,5 euros en France Metropolitaine</p></TD>
+    
+    <TD><p><a href='produit.'>Licorne volante</a><br /><img id='imageminiature' src='licorne.png' /><br/><img id='etoiles' src='5etoiles.png' /><br/> 15 euros<br/> livraison: 0,5 euros en France Metropolitaine</p></TD>
+    <TD><p><a href='produit.'>Licorne volante</a><br /><img id='imageminiature' src='licorne.png' /><br/><img id='etoiles' src='5etoiles.png' /><br/> 15 euros<br/> livraison: 0,5 euros en France Metropolitaine</p></TD>
+</TR>
+<TR>
+    <TD><p><a  href='produit.'>Licorne volante</a><br /><img id='imageminiature' src='licorne.png' /><br/><img id='etoiles' src='5etoiles.png' /><br/> 15 euros<br/> livraison: 0,5 euros en France Metropolitaine</p></TD>
+    
+    <TD><p><a  href='produit.'>Licorne volante</a><br /><img id='imageminiature' src='licorne.png' /><br/><img id='etoiles' src='5etoiles.png' /><br/> 15 euros<br/> livraison: 0,5 euros en France Metropolitaine</p></TD>
+    <TD><p><a  href='produit.'>Licorne volante</a><br /><img id='imageminiature' src='licorne.png' /><br/><img id='etoiles' src='5etoiles.png' /><br/> 15 euros<br/> livraison: 0,5 euros en France Metropolitaine</p></TD>
+</TR>
+<TR>
+    <TD><p><a  href='produit.'>Licorne volante</a><br /><img id='imageminiature' src='licorne.png' /><br/><img id='etoiles' src='5etoiles.png' /><br/> 15 euros<br/> livraison: 0,5 euros en France Metropolitaine</p></TD>
+    
+    <TD><p><a  href='produit.'>Licorne volante</a><br /><img id='imageminiature' src='licorne.png' /><br/><img id='etoiles' src='5etoiles.png' /><br/> 15 euros<br/> livraison: 0,5 euros en France Metropolitaine</p></TD>
+    <TD><p><a  href='produit.'>Licorne volante</a><br /><img id='imageminiature' src='licorne.png' /><br/><img  id='etoiles' src='5etoiles.png' /><br/> 15 euros<br/> livraison: 0,5 euros en France Metropolitaine</p></TD>
+</TR>
+<TR>
+    <TD><p><a  href='produit.'>Licorne volante</a><br /><img id='imageminiature' src='licorne.png' /><br/><img id='etoiles'  src='5etoiles.png' /><br/> 15 euros<br/> livraison: 0,5 euros en France Metropolitaine</p></TD>
+    
+    <TD><p><a  href='produit.'>Licorne volante</a><br /><img id='imageminiature' src='licorne.png' /><br/><img id='etoiles' src='5etoiles.png' /><br/> 15 euros<br/> livraison: 0,5 euros en France Metropolitaine</p></TD>
+    <TD><p><a  href='produit.'>Licorne volante</a><br /><img id='imageminiature' src='licorne.png' /><br/> <img id='etoiles' src='5etoiles.png' /><br/>15 euros<br/> livraison: 0,5 euros en France Metropolitaine</p></TD>
+</TR>
+</TABLE>
+
+
+
+</body>
+
+
+<footer></footer>
+
+
+</html>
