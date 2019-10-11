9fc45849031361a146ff7861d02cf8c7539bae3f
TagXy <anthocoups@gmail.com>
2019-10-11 15:21:54 +0200

    create_account
    Adding addresses in form.

--- create_account.php
+++ create_account.php
@@ -20,12 +20,14 @@
 	<br> 
 	<form class="InscriptionForm">
 		<input class="InscriptionField" type="text" name="pseudo" placeholder="Pseudo"><br><br>
+		<input class="InscriptionField" type="password" name="password" placeholder="Password"><br><br>
+		<input class="InscriptionField" type="password" name="password_two" placeholder="Write your password again"><br><br>
 		<input class="InscriptionNameField" type="text" name="Surname" placeholder="Surname">
 		<input class="InscriptionNameField" type="text" name="Name" placeholder="Name"><br><br>
 		<input class="InscriptionField" type="text" name="email" placeholder="E-mail address"><br><br>
 		<input class="InscriptionField" type="tel" name="phone" placeholder="Phone number" pattern="[0-9]{2}.[0-9]{2}.[0-9]{2}.[0-9]{2}.[0-9]{2}" required><br><br>
-		<input class="InscriptionField" type="password" name="password" placeholder="Password"><br><br>
-		<input class="InscriptionField" type="password" name="password_two" placeholder="Write your password again"><br><br>
+		<input class="InscriptionField" type="text" name="address1" placeholder="Street, number..."><br><br>
+		<input class="InscriptionField" type="text" name="address2" placeholder="Appartement number..."><br><br>
 		We need your Favorite color:
 		<input class="InscriptionColor" type="color" placeholder="000000"><br><br>
 		<input class="InscriptionButton" type="submit" value="Sign in">
