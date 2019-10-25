db41f0fbaf0dd0b6a5716da6640923d6be1824dc
Grand Corbeau <qboens@hotmail.fr>
2019-10-25 21:55:30 +0200

    correction_error_get

--- Product_page.php
+++ Product_page.php
@@ -14,21 +14,36 @@
 <body  id="body_princ">
 		
 		<?php
-			$id_prod = $_GET["id_prod"];
-			
-			$prod_data = array_slice(getProductById($id_prod),0,1);
+			if(empty($_GET))
+			{
+				$id_prod = -1000;
 
-			$prod_com = array_slice(getCommentsByProductId($id_prod),0,1);
+			}
+			else
+			{
+				$id_prod = $_GET["id_prod"];
 			
+				$prod_data = array_slice(getProductById($id_prod),0,1);
+
+				$prod_com = array_slice(getCommentsByProductId($id_prod),0,1);
+			}
 		?>
 		<section class="information">
 			<article id="zone_image">
 				<img src="src/Pictures/<?php
-						echo $prod_data[0]["image"];
-					?>" id="img_prod" alt="Ou est mon dragon?"  >
+						if($id_prod != -1000){
+							echo $prod_data[0]["image"];
+						}
+					?>" id="img_prod" alt="Pas de produit"  >
 				<p id="nom_prod">
 					<?php
-						echo $prod_data[0]["name"];
+						if($id_prod != -1000){
+							echo $prod_data[0]["name"];
+						}
+						else
+						{
+							echo "Pas de produit";
+						}
 					?>
 				</p>
 			</article>
@@ -38,14 +53,26 @@
 			<aside class="zone_info">
 				<p id="prix_prod">Prix : 
 					<?php
-						echo $prod_data[0]["unit_price"]." €";
+						if($id_prod != -1000){
+							echo $prod_data[0]["unit_price"]." €";
+						}
+						else
+						{
+							echo "Pas de produit";
+						}
 					?>
 				</p>
 
 				<p class="titre_desc">Description</p>
 				<p id="desc_prod">
 					<?php
-						echo $prod_data[0]["description"];
+						if($id_prod != -1000){
+							echo $prod_data[0]["description"];
+						}
+						else
+						{
+							echo "Aucun produit a decrire";
+						}
 					?>
 
 				</p>
@@ -53,7 +80,13 @@
 				<p class="titre_desc">Fournisseur</p>
 				<p id="fournis">
 					<?php
-						echo $prod_data[0]["supplier"];
+						if($id_prod != -1000){
+							echo $prod_data[0]["supplier"];
+						}
+						else
+						{
+							echo "Pas de produit";
+						}
 					?>
 
 				</p>
@@ -70,11 +103,18 @@
 			<p class="comment">
 
 				<?php
-					foreach ($prod_com as $com)
-					{ 
-						echo "Redige par : ".$com['username']."<br>";
-						echo "Note : ".$com['rating'];
-						echo "<br>".$com['comment'];
+					if($id_prod != -1000){
+						foreach ($prod_com as $com)
+						{ 
+							echo "Redige par : ".$com['username']."<br>";
+							echo "Note : ".$com['rating'];
+							echo "<br>".$com['comment'];
+						}
+
+					}
+					else
+					{
+						echo "Aucun produit";
 					}
 				?>
 			</p>
