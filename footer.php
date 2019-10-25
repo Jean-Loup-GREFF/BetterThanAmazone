0eab44b5c17a42933af6230e5792f2e54fae3f98
jean-loup <jeanloup.greff@gmail.com>
2019-10-25 16:28:23 +0200

    Missing links

--- footer.php
+++ footer.php
@@ -3,10 +3,9 @@
 
     <link rel="stylesheet" href="style_web_site_complet.css" />
     <ul class="premierul">
-     <li class="premierli"><a class="premierlia" href="default.asp">Test</a></li>
-     <li class="premierli"><a class="premierlia" href="news.asp">Contact</a></li>
-     <li class="premierli"><a class="premierlia" href="SearchProductList.php">About</a></li>
-     <li class="premierli"><a class="premierlia" href="about.asp">About</a></li>
+     <li class="premierli"><a class="premierlia" href="https://www.lemonde.fr/">Contact</a></li>
+     <li class="premierli"><a class="premierlia" href="https://www.merriam-webster.com/dictionary/about">About</a></li>
+     <li class="premierli"><a class="premierlia" href="https://www.amazon.com/gp/BIT/ref=footer_bit_v2_us_A0029?bitCampaignCode=A0029">contact</a></li>
     </ul>
     <p><br/><img id='imagebanderole' src='src/pictures/imagelivraison.png' /></p>
 </footer>
