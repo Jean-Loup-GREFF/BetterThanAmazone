8bc8f6080396f22f651a8cfa98c718b0971c20a0
Gabriel <takedadesmullier@gmail.com>
2019-10-25 16:00:51 +0200

    Premier Test Dynamique Recherche Produit
    à tester sur un autre PC

--- SearchProductList.php
+++ SearchProductList.php
@@ -1,22 +1,23 @@
-<html>
+<html> 
 <head>
-    <link rel="stylesheet" href="src/CSS/style_web_site_complet.css" />
+    <link rel="stylesheet" href=style_web_site_complet.css />
     <?php
-			include('header.php');
+            include('header.php');
+            include("connexion.php")
 		?>
 </head>
 <body >
 
-
-
+    
+    
     <div id='searchbox'>
         <h1>Recherche</h1>
-    <form><p>
-    Rechercher un produit:
+    <form action="SearchProductList.php" method="post"><p>
+    Rechercher un produit: 
     <input type="text" name="search" class=string placeholder="Licorne volante..."/><br />
-</p></form>
-    <form><p>
-
+</p>
+    <p>
+    
     <label for="budget">Votre budget:</label>
        <select name="budget" id="budget">
            <option value="peuimporte">C'est peu important</option>
@@ -27,11 +28,11 @@
            <option value="entre30et40">30-40 euros</option>
            <option value="entre40et50">40-50 euros</option>
            <option value="plusde50">plus de 50 euros</option>
-       </select>
+       </select>  
 </p>
-</form>
-<form><p>
 
+<p>
+    
     <label for="categorie">Catégorie:</label>
        <select name="categorie" id="categorie">
            <option value="peuimportecategorie">Tout les produits</option>
@@ -41,40 +42,45 @@
            <option value="baguettesmagiques">Baguettes de sorcier</option>
            <option value="livredesorts">Livre de Sortilèges</option>
            <option value="creatures">Créatures</option>
-       </select>
+       </select>  
 </p>
-</form>
-<form><p><input type="submit" value="Rechercher" /></p></form>
-</div>
 
+<p><input type="submit" name="valider" value="Rechercher" /></p></form>
+
+<p><?php 
+         if (isset($_POST["valider"])) {
+         $searchEntries=True;
+         $searchString = htmlentities($_POST["search"],ENT_QUOTES, "UTF-8");
+         $searchPrice=htmlentities($_POST["budget"],ENT_QUOTES, "UTF-8");
+         $searchCategorie=htmlentities($_POST["categorie"],ENT_QUOTES, "UTF-8");}
+         else{
+             $searchEntries=False;
+         }
+
+
+
+
+
+ ?></p>
+</div>
 <TABLE>
 <CAPTION> </CAPTION>
+<?php if ($searchEntries==False){
+    $productsList=getAllProducts();
+    $column=0;
+    foreach($productsList as $product){
+        echo '<TD><p ><br/><a id="productname" href="produit">'.$product["name"].'</a><br /><br/><img id="imageminiature" "src="'.$product["image"].' /><br /><img id="etoiles" src="5etoiles.png" /><br/> '.$product[unit_price].' euros<br/><br/> </p></TD>';
 
-<TR>
-    <TD><p ><br/><a id='productname' href='produit.'>Licorne volante</a><br /><br/><img id='imageminiature' src='src/pictures/licorne.png' /><br /><img id='etoiles' src='src/pictures/5etoiles.png' /><br/> 15 euros<br/><br/> </p></TD>
+        $column+=1;
+        if ($column%3==0){
+            echo '</TR><TR>';
+            $column=0;
 
-    <TD><p ><br/><a  id='productname' href='produit.'>Licorne volante</a><br /><br/><img id='imageminiature' src='src/pictures/licorne.png' /><br/><img id='etoiles' src='src/pictures/5etoiles.png' /><br/> 15 euros<br/> <br/></p></TD>
-    <TD><p ><br/><a id='productname' href='produit.'>Licorne volante</a><br /><br/><img id='imageminiature' src='src/pictures/licorne.png' /><br/><img id='etoiles' src='src/pictures/5etoiles.png' /><br/> 15 euros<br/><br/> </p></TD>
-</TR>
-<TR>
-    <TD><p><br/><a id='productname' href='produit.'>Licorne volante</a><br /><br/><img id='imageminiature' src='src/pictures/licorne.png' /><br/><img id='etoiles' src='src/pictures/5etoiles.png' /><br/> 15 euros<br/> <br/></p></TD>
+        }
+    }
 
-    <TD><p><br/><a id='productname' href='produit.'>Licorne volante</a><br /><br/><img id='imageminiature' src='src/pictures/licorne.png' /><br/><img id='etoiles' src='src/pictures/5etoiles.png' /><br/> 15 euros<br/> <br/></p></TD>
-    <TD><p ><br/><a id='productname' href='produit.'>Licorne volante</a><br /><br/><img id='imageminiature' src='src/pictures/licorne.png' /><br/><img id='etoiles' src='src/pictures/5etoiles.png' /><br/> 15 euros<br/><br/> </p></TD>
-</TR>
-<TR>
-    <TD><p ><br/><a id='productname' href='produit.'>Licorne volante</a><br /><br/><img id='imageminiature' src='src/pictures/licorne.png' /><br/><img id='etoiles' src='src/pictures/5etoiles.png' /><br/> 15 euros<br/> <br/></p></TD>
+} ?>
 
-    <TD><p ><br/><a id='productname' href='produit.'>Licorne volante</a><br /><br/><img id='imageminiature' src='src/pictures/licorne.png' /><br/><img id='etoiles' src='src/pictures/5etoiles.png' /><br/> 15 euros<br/><br/> </p></TD>
-    <TD><p ><br/><a id='productname' href='produit.'>Licorne volante</a><br /><br/><img id='imageminiature' src='src/pictures/licorne.png' /><br/><img  id='etoiles' src='src/pictures/5etoiles.png' /><br/> 15 euros<br/> <br/></p></TD>
-</TR>
-<TR>
-    <TD><p ><br/><a id='productname' href='produit.'>Licorne volante</a><br /><br/><img id='imageminiature' src='src/pictures/licorne.png' /><br/><img id='etoiles'  src='src/pictures/5etoiles.png' /><br/> 15 euros<br/><br/> </p></TD>
-
-    <TD><p ><br/><a id='productname' href='produit.'>Licorne volante</a><br /><br/><img id='imageminiature' src='src/pictures/licorne.png' /><br/><img id='etoiles' src='src/pictures/5etoiles.png' /><br/> 15 euros<br/> <br/></p></TD>
-    <TD><p ><br/><a id='productname' href='produit.'>Licorne volante</a><br /><br/><img id='imageminiature' src='src/pictures/licorne.png' /><br/> <img id='etoiles' src='src/pictures/5etoiles.png' /><br/>15 euros<br/> <br/></p></TD>
-</TR>
-</TABLE>
 
 
 
