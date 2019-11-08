a21cd8f67171a259696c516704d60942ed5b10f8
Gabriel <takedadesmullier@gmail.com>
2019-11-08 14:04:39 +0100

    Page pour se connecter
    oubliée + mise à jour du header

--- header.php
+++ header.php
@@ -14,6 +14,7 @@
        <li class = "essai2"><a class = "menu_reference" href="SearchProductList.php">Products</a></li>
        <li class = "essai2"><a class = "menu_reference" href="https://fr.wikipedia.org/wiki/Donjons_et_Dragons">About</a></li>
        <li class = "essai2"><a class = "menu_reference" href="create_account.php">Sign in</a></li>
+       <li class = "essai2"><a class = "menu_reference" href="connect.php">Sign up</a></li>
        <li class = "essai2"><a class = "menu_reference" href="cart.php">Your Cart</a></li>
       </ul>
 
