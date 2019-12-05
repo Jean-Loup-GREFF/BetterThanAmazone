ab9e40f94520e02c43b7ef3f6a8b756a7d395bf3
Matthieu <mathiascarbor49@gmail.com>
2019-12-05 19:49:50 +0100

    cart???

--- cart.php
+++ cart.php
@@ -28,7 +28,19 @@
 		
 		if(isset($_POST["deleteproduct"])){
 			$cartOrder = array_slice(getCartOrderOfUser($_SESSION["userID"]), 0, 1);
-			$productOrder = array_slice(getOrderProductOfOrderAndSpecificProduct($cartOrder[0]["id"], $_POST["deleteproduct"]), 0, 1);
+			$userInfo = array_slice(getUserById($user), 0, 1);
+			if(Count($cartOrder) > 0 && Count($userInfo))
+			{
+				insertToBDD("UPDATE`orders` SET type`=`ORDER`,`status`=`WAIT_FOR_BILL` WHERE `id`=\"".$cartOrder["id"]."\"");
+				insertToBDD("INSERT INTO `orders` (`user_id`, `type`, `status`, `amount`, `billing_adress_id`, `delivery_adress_id`)"." VALUES (\"".$user."\", \"CART\", \"CART\", \"0\", \"".$userInfo[0]["billing_adress_id"]."\", \"".$userInfo[0]["delivery_adress_id"]."\")");
+			}
+		}
+		
+		if(isset($_POST["validateCart"])){
+			$cartOrder = array_slice(getCartOrderOfUser($_SESSION["userID"]), 0, 1);
+			
+			
+			
 			if(Count($productOrder) > 0)
 			{
 				if(isset($_POST["deletionQuantity"])){
@@ -37,19 +49,6 @@
 				else {
 					addToCart($_SESSION["userID"],$_POST["deleteproduct"], -1 * $productOrder[0]["quantity"]);
 				}
-			}
-		}
-		
-		if(isset($_POST["validateCart"])){
-			$cartOrder = array_slice(getCartOrderOfUser($_SESSION["userID"]), 0, 1);
-			$userInfo = array_slice(getUserById($_SESSION["userID"]), 0, 1);
-			
-			
-			if(Count($cartOrder) > 0 && $userInfo > 0)
-			{
-				insertToBDD("UPDATE `orders` SET `type`=\"ORDER\",`status`=\"WAIT_FOR_BILL\" WHERE `id`=\"".$cartOrder[0]["id"]."\"");
-				insertToBDD("INSERT INTO `orders` (`user_id`, `type`, `status`, `amount`, `billing_adress_id`, `delivery_adress_id`)"." VALUES (\"".$user."\", \"CART\", \"CART\", \"0\", \"".$userInfo[0]["billing_adress_id"]."\", \"".$userInfo[0]["delivery_adress_id"]."\")");
-			
 			}
 		}
 		
@@ -69,12 +68,17 @@
 			<div id="items">
 				<table>
 					<caption><h2 id='titlegeneral'>Your Cart</h2></caption>
+					<?php if(count($cart) == 0){ ?>
+						<tr>
+							<td><p id='cart'>Your Cart is empty</p></td>
+						</tr>
+					<?php } ?>
 					<?php foreach ($cart as $value) {?>
 					<tr>
 						<td><img src="src/pictures/<?php echo $value["image"];?>" id="cart" widht=200 height="300"></td>
 						<td><p id='cart'><?php echo $value["name"];?><br />
 						<form action="" method="post">
-							<button class = "menu_reference" type="submit" name="deleteproduct" value=<?php echo $value["id"];?> >Supprimer</button>
+							<button type="submit" name="deleteproduct" value=<?php echo $value["id"];?> >Supprimer</button>
 						</form>
 						<td>Quantity: <?php echo $value["quantity"];?></td>
 						<td><?php echo $value["unit_price"]*$value["quantity"];?></td>
@@ -83,11 +87,10 @@
 				</table>
 			</div>
 		</section>
-		<?php if(Count($cart) > 0) {?>
-		<form align="right" action="" method="post">
-			<br/><br/><br/><br/><br/><button class = "menu_reference" type="submit" name="validateCart" value=true >Commander</button>
+		
+		<form action="" method="post">
+			<button type="submit" name="validateCart" value=true >Commander</button>
 		</form>
-		<?php }?>
 	</div>
 	<div id="footer">
 	</div>
